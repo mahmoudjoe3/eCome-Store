9bab4d984d2c3987972b2cb32c089cfad06faf35
mahmoud youssef <49236858+mahmoudjoe3@users.noreply.github.com>
2021-01-09 23:17:06 +0200

    update haveNetworkConnection method

--- app/src/main/java/com/mahmoudjoe3/eComStore/Logic/MyLogic.java
+++ app/src/main/java/com/mahmoudjoe3/eComStore/Logic/MyLogic.java
@@ -2,26 +2,27 @@ package com.mahmoudjoe3.eComStore.Logic;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.os.Build;
 
 
 public class MyLogic {
 
 
-    public static boolean haveNetworkConnection(Context context) {
-        boolean haveConnectedWifi = false;
-        boolean haveConnectedMobile = false;
-
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
-        for (NetworkInfo ni : netInfo) {
-            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
-                if (ni.isConnected())
-                    haveConnectedWifi = true;
-            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
-                if (ni.isConnected())
-                    haveConnectedMobile = true;
+    public static boolean haveNetworkConnection(Context application) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) application.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Network nw = connectivityManager.getActiveNetwork();
+            if (nw == null) return false;
+            NetworkCapabilities actNw = connectivityManager.getNetworkCapabilities(nw);
+            return actNw != null &&
+                    (actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+                            || actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR));
+        } else {
+            NetworkInfo nwInfo = connectivityManager.getActiveNetworkInfo();
+            return nwInfo != null && nwInfo.isConnected();
         }
-        return haveConnectedWifi || haveConnectedMobile;
     }
 }
